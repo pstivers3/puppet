eae11131c37ba50394abdecd24aac7d368e5dde2
Paul Stivers <paul.stivers@fidelitylife.com>
2015-12-25 04:54:37 -0800

    fix indent style error

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -1,3 +1,6 @@
+# pull updates from Git and apply puppet code
+# run a script in cron to do same
+
 class puppet {
 
   file { '/usr/local/bin/papply':
@@ -13,7 +16,7 @@ class puppet {
   cron { 'run-puppet':
 #    ensure  => 'present',
     ensure  => 'absent',
-    user => 'ubuntu',
+    user    => 'ubuntu',
     command => '/usr/local/bin/pull-updates',
     minute  => '*/10',
     hour    => '*',
