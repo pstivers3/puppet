d86e210137fb574beb8f1e94d0546422914d2e48
Paul Stivers <paul.stivers@fidelitylife.com>
2015-12-28 21:09:36 -0800

    add a tag

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,4 +6,13 @@ node 'pc2' {
   include puppet
   include memcached
   include admin::ntp
+
+  if tagged('admin::ntp') {
+    notify { 'This node is running NTP': }
+  }
+
+  if tagged('admin') {
+    notify { 'This node includes at least one class from the admin module': }
+  }
+
 }
