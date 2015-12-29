a0f0c1a265abcd1180ce8cf03bf03773eacb20cc
Paul Stivers <pstivers3@comcast.net>
2015-12-29 00:19:59 +0000

    try diff syntax for dependency chain. Didn't work.

--- modules/admin/manifests/ntp.pp
+++ modules/admin/manifests/ntp.pp
@@ -13,5 +13,10 @@ class admin::ntp {
     source  => 'puppet:///modules/admin/ntp.conf',
     notify  => Service['ntp'],
     require => Package['ntp'],
+
+    # Another way to do it
+    # Package['ntp'] -> File['/etc/ntp.conf'] ~> Service['ntp'], # throws a syntax error 
+    # File['/etc/ntp.conf'] ~> Package['ntp'] -> Service['ntp'], # swap position of File and Package as a test. Same syntax error.
   }
+
 }
