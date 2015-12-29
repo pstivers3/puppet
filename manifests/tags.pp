02fefe92082d660d857a682bdfa5d4b73816e28a
Paul Stivers <pstivers3@comcast.net>
2015-12-29 13:06:21 +0000

    initial commit

---
+++ manifests/tags.pp
@@ -0,0 +1,28 @@
+node 'bitfield_server' {
+}
+
+node 'pc2', 'pc1' inherits 'bitfield_server' {
+  include admin::ntp
+
+  if tagged('pc2') {
+    notify { 'This is pc2. This will succeed.': }
+  }
+
+  if tagged('bitfield_server') {
+    notify { 'This is bitfield_server. This will also succeed.': }
+  }
+
+  if tagged('admin::ntp') {
+    notify { 'This node is running NTP': }
+  }
+
+  if tagged('admin') {
+    notify { 'This node includes at least one class from the admin module': }
+  }
+  
+  tag('big-server') # set a tag
+  if tagged('big-server') {
+    notify { 'Big server detected. Adding extra workload': }
+  }
+}
+
