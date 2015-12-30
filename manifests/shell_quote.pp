95517b987e53458100aef1b0591a77ebe9e7243b
Paul Stivers <pstivers3@comcast.net>
2015-12-30 18:19:13 +0000

    initial commit

--- manifests/shell_quote.pp
+++ manifests/shell_quote.pp
@@ -2,7 +2,7 @@ node 'pc1', 'pc2' {
 
   $dir1 = 'test dir1'
   $dir2  = 'test dir2'
-  $argstring = shellquote($dir1, $dir2)
+  $argstring = shellquote($dir1, $dir2) # place double quotes around each variable, $dir1 and $dir2.
   $command = "/bin/mv ${argstring}"
   notify { $command: }
 
