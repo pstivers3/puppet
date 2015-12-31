a6069082560b8e72531f6dce5384444ea1c3c707
Paul Stivers <pstivers3@comcast.net>
2015-12-31 22:30:58 +0000

    minor edits

--- manifests/hiera.pp
+++ manifests/hiera.pp
@@ -3,8 +3,8 @@ node 'FLAM06', 'pc1', 'pc2' {
   $message = hiera('magic_word')
   notify { $message: }
 
-#  $message2 = hiera('greeting')
-#  notify { $message2: }
+  $message2 = hiera('greeting')
+  notify { $message2: }
 
 }
 
