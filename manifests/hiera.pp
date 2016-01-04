a54e2ac403ba4b99157d8dd20f7ced4ec810eb3f
Paul Stivers <pstivers3@comcast.net>
2016-01-04 08:41:13 +0000

    add hiera data

--- manifests/hiera.pp
+++ manifests/hiera.pp
@@ -1,10 +1,13 @@
 node 'pc1', 'pc2' {
 
-  $message = hiera('magic_word')
-  notify { $message: }
+  $magic_word = hiera('magic_word')
+  notify { $magic_word: }
 
-  $message2 = hiera('greeting')
-  notify { $message2: }
+  $node_greeting = hiera('node_greeting')
+  notify { $node_greeting: }
 
+  $is_virtual_greeting = hiera('is_virtual_greeting')
+  notify { $is_virtual_greeting: }
+  
 }
 
