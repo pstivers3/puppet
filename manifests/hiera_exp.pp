6a9441d9e3c666afbaca0bd61f7acfed4275099b
Paul Stivers <pstivers3@comcast.net>
2016-01-09 05:00:39 +0000

    edit eyaml test

--- manifests/hiera_exp.pp
+++ manifests/hiera_exp.pp
@@ -1,16 +1,24 @@
 node 'pc1', 'pc2' {
 
-  $magic_word = hiera('magic_word')
-  notify { $magic_word: }
+  ## test eyaml
+  $my_secret = hiera('my_secret')
+  notify { "==> \$my_secret: $::my_secret": } 
+ 
+  if $::mysecret == 'mysecretstuff_edited' {
+    notify { "==> eyaml works!": }
+  } elsif $::mysecret == '' {
+    notify { "==> \$my_secret is empty": }
+  } 
+  ## end test eyaml
 
+  $magic_word = hiera('magic_word')
+  notify { "==> \$magic_word: $magic_word": }
+  
   $node_greeting = hiera('node_greeting')
-  notify { $node_greeting: }
+  notify { "==> \$node_greeting: $node_greeting": }
 
   $is_virtual_greeting = hiera('is_virtual_greeting')
-  notify { "is_virtual_greeting: $is_virtual_greeting": }
+  notify { "==> \$is_virtual_greeting: $is_virtual_greeting": }
 
-  $my_secret = hiera('my_secret')
-  notify { $my_secret: }
-  
 }
 
