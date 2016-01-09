096bb8611f49518ecc7670aafbb78c8b6ee114d3
Paul Stivers <pstivers3@comcast.net>
2016-01-09 11:09:51 +0000

    fixed

--- manifests/param.pp
+++ manifests/param.pp
@@ -1,16 +1,13 @@
-# doesn't work 
-# puppet apply param.pp throws an error
+class param_test ($param1, $param2) {
+  notify { "param1 is ${param1}": }
+  notify { "param2 is ${param2}": }
+}
 
 node 'pc1', 'pc2' {
 
-  class param_test ($param1, $param2) {
-    notify { "param1 is ${param1}": }
-    notify { "param2 is ${param2}": }
-  }
-
   class { 'param_test':
-    param1 = 'woo!',
-    param2 = 'hoo!',
+    param1 => 'woo!',
+    param2 => 'hoo!',
   }
 
 }
